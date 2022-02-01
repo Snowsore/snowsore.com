46c4179e8971e9365e34b3f44b373f051b88269d
Snowsore <snow_sore@hotmail.com>
2022-02-01 17:23:10 +0800

    Set iframe size

--- app/src/routers/Home.jsx
+++ app/src/routers/Home.jsx
@@ -7,12 +7,11 @@ export default function Routers() {
         Welcome to Snowsore.com
         <br />
         <iframe
-          width="560"
-          height="315"
+          className="w-full h-full"
           src="https://www.youtube.com/embed/dQw4w9WgXcQ"
           title="YouTube video player"
           frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         />
       </Box>
