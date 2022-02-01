a7bdf3da24370347df66b0c16f977744f6262a00
Snowsore <snow_sore@hotmail.com>
2022-02-01 17:07:34 +0800

    Add nerver gonna give you up

--- app/src/routers/Home.jsx
+++ app/src/routers/Home.jsx
@@ -3,7 +3,19 @@ import { Frame, Box } from "@components/Basic";
 export default function Routers() {
   return (
     <Frame>
-      <Box title="Welcome">Welcome to Snowsore.com</Box>
+      <Box title="Welcome">
+        Welcome to Snowsore.com
+        <br />
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+      </Box>
     </Frame>
   );
 }
