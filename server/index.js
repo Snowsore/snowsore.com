ba5762bcb558f24fd12179b157fc10850aae95e0
Snowsore <snow_sore@hotmail.com>
2022-02-08 02:09:06 +0800

    Add server public

--- server/index.js
+++ server/index.js
@@ -2,9 +2,31 @@
 
 const express = require("express");
 const app = express();
+const session = require("express-session");
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    cookie: { maxAge: 60000 },
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ msg: "good" });
+});
+
+app.post("/", (req, res) => {
+  const id = req.body.id;
+  const passwd = req.body.passwd;
+  res.redirect("/");
+});
+
+app.use((req, res, next) => {
+  if (req.session.user_id) next();
+  else res.redirect("/");
 });
 
 app.listen(8080, () => {
