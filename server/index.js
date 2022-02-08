7c86ec529ebd555ce48af108202fa356d0bcd822
Snowsore <snow_sore@hotmail.com>
2022-02-08 11:25:34 +0800

    Change server public folder to app folder

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 
-app.use(express.static("public"));
+app.use(express.static("../app/build"));
 app.use(express.json());
 
 app.use(
